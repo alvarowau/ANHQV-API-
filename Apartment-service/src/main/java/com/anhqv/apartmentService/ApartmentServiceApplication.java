2f47cf5116bd55c8544e8a93f404610d23e0a0a1
alvarowau <alvarowau12@proton.me>
2025-06-14 22:49:10 +0200

    Initialize apartment-service with basic service structure and update docker-compose
    - Added initial folder and package structure for apartment-service
    - Included service skeleton (interfaces and placeholders)
    - Modified docker-compose.yml to add apartment-db service with its configuration

---
+++ Apartment-service/src/main/java/com/anhqv/apartmentService/ApartmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.anhqv.apartmentService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApartmentServiceApplication.class, args);
+	}
+
+}
